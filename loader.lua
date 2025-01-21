d3339ed2c7bb1529818bf31a415050335034b54e
Kvkfjf <lowcycheaterowaxelcraft@gmail.com>
2025-01-21 13:19:05 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -33,6 +33,24 @@ local Scripts = {
     ["Race Clicker"] = "RaceClicker.lua"
 }
 
+-- GitHub Repository Information
+local GitHubInfo = {
+    Owner = "Kvkfjf",
+    Repo = "Scripts",
+    Branch = "main"
+}
+
+-- Function to get raw GitHub content URL
+local function GetGitHubUrl(filename)
+    return string.format(
+        "https://raw.githubusercontent.com/%s/%s/%s/%s",
+        GitHubInfo.Owner,
+        GitHubInfo.Repo,
+        GitHubInfo.Branch,
+        filename
+    )
+end
+
 -- Load Kavo UI Library
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("🧈 Butt3rL3aks Hub | Loader", "Ocean")
@@ -49,12 +67,11 @@ local InfoSection = InfoTab:NewSection("Information")
 for scriptName, scriptFile in pairs(Scripts) do
     MainSection:NewButton(scriptName, "Load " .. scriptName .. " script", function()
         -- Show loading notification
-        Notify("Loading...", "Loading script for " .. scriptName, 3)
+        Notify("Loading...", "Loading " .. scriptName .. "...", 3)
         
-        -- Load game script from local file
-        local scriptPath = "ButterLeaks/Games/" .. scriptFile
+        -- Load script from GitHub
         local success, err = pcall(function()
-            loadstring(readfile(scriptPath))()
+            loadstring(game:HttpGet(GetGitHubUrl(scriptFile)))()
         end)
         
         if not success then
@@ -71,6 +88,10 @@ InfoSection:NewLabel("Script by: kvkfjf")
 InfoSection:NewLabel("UI Library: Kavo UI")
 InfoSection:NewButton("Copy Discord", "Copies Discord invite to clipboard", function()
     setclipboard("discord.gg/S7qUMBNq6f")
+end)
+InfoSection:NewButton("GitHub Repository", "Opens GitHub repository", function()
+    setclipboard("https://github.com/Kvkfjf/Scripts")
+    Notify("GitHub", "Repository URL copied to clipboard!", 3)
 end)
 
 -- Welcome Message
